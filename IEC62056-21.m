df1a6ed48318b56e14ad5a50d7b8f5c3f008b177
st0ff3r <stoffer@skulp.net>
2014-06-21 00:03:38 +0200

    start of multical 2001

--- IEC62056-21.m
+++ IEC62056-21.m
@@ -57,75 +57,102 @@
         return;
     }
 
-    if (bytes[0] == '/') {
-        // ACK
+    if ((bytes[0] == 0x02) | (bytes[0] == '/')) {
         bytes = theFrame.bytes;
-        //[self.responseData setObject:[NSData dataWithBytes:bytes length:theFrame.length] forKey:@"ack"];
-        [self.responseData setObject:theFrame forKey:@"ack"];
-        self.frameReceived = YES;
-    }
-    else if (bytes[0] == 0x02) {
-        // BCC
-        NSRange range = NSMakeRange(theFrame.length - 2, 1);
-        uint8_t bcc = bytes[theFrame.length - 1];
-        //[self.responseData setObject:[self.frame subdataWithRange:range] forKey:@"bcc"];
 
-        // Data block
-       range = NSMakeRange(1, [theFrame length] - 2);
-        NSData *dataBlock = [theFrame subdataWithRange:range];
-        
-        // check bcc
-        uint8_t calculatedBCC = 0;
-        for (unsigned int i = 0; i < dataBlock.length; i++) {
-            range = NSMakeRange(i, 1);
-            const char *b = [[dataBlock subdataWithRange:range] bytes];
-            calculatedBCC += b[0];
-            calculatedBCC &= 0x7f;  // 7 bit value
+        // Ident
+        NSString *str = [[NSString alloc] initWithData:theFrame encoding:NSUTF8StringEncoding];
+        NSError *regexError;
+        NSRegularExpression *regex = [NSRegularExpression regularExpressionWithPattern:@"^[/](.*?)\r\n(\x02)?" options:NSRegularExpressionDotMatchesLineSeparators error:&regexError];
+        if (regexError) {
+            NSLog(@"%@",regexError.localizedDescription);
         }
-        if (calculatedBCC == bcc) {
-            NSLog(@"bcc ok");
-            frameReceived = YES;
-            
-            // parse data
-            NSString *str = [[NSString alloc] initWithData:dataBlock encoding:NSUTF8StringEncoding];
-            NSRegularExpression *regex = [NSRegularExpression regularExpressionWithPattern:@"(.*?)[(](.*?)(?:[*](.*?))?[)]" options:0 error:NULL];
-            NSArray* matches = [regex matchesInString:str options:0 range:NSMakeRange(0, str.length)];
-            for (NSTextCheckingResult *match in matches) {
-                NSRange ridGroup = [match rangeAtIndex:1];
-                NSRange valueGroup = [match rangeAtIndex:2];
-                NSRange unitGroup = [match rangeAtIndex:3];
-                
-                if (ridGroup.length && valueGroup.length) {
-                    NSString *rid = [str substringWithRange:ridGroup];
-                    NSMutableString *valueString = [[str substringWithRange:valueGroup] mutableCopy];
-                    [self.responseData setObject:[[NSMutableDictionary alloc] init] forKey:rid];
+        NSTextCheckingResult *match = [regex firstMatchInString:str options:kNilOptions range:NSMakeRange(0, str.length)];
+        NSRange range = [match rangeAtIndex:1];
+        if (range.location != NSNotFound) {
+            NSData *identData = [theFrame subdataWithRange:range];
+            NSString *ident = [[NSString alloc] initWithData:identData encoding:NSUTF8StringEncoding];
+            [self.responseData setObject:ident forKey:@"ident"];
+        }
+        
+        // Data block
+        str = [[NSString alloc] initWithData:theFrame encoding:NSUTF8StringEncoding];
+        regex = [NSRegularExpression regularExpressionWithPattern:@"\x02(.*).$" options:NSRegularExpressionDotMatchesLineSeparators error:&regexError];
+        if (regexError) {
+            NSLog(@"%@",regexError.localizedDescription);
+        }
+        match = [regex firstMatchInString:str options:kNilOptions range:NSMakeRange(0, str.length)];
+        range = [match rangeAtIndex:1];
+        if (range.location != NSNotFound) {
+            NSData *dataBlock = [theFrame subdataWithRange:range];
+            if (dataBlock.length) {
+                // get bcc
+                range = NSMakeRange(theFrame.length - 2, 1);
+                uint8_t bcc = bytes[theFrame.length - 1];
+
+                // calculate bcc
+                uint8_t calculatedBCC = 0;
+                for (unsigned int i = 0; i < dataBlock.length; i++) {
+                    range = NSMakeRange(i, 1);
+                    const char *b = [[dataBlock subdataWithRange:range] bytes];
+                    calculatedBCC += b[0];
+                    calculatedBCC &= 0x7f;  // 7 bit value
+                }
+                if (calculatedBCC == bcc) {
+                    NSLog(@"bcc ok");
+                    frameReceived = YES;
                     
-                    if (unitGroup.length) {
-                        NSString *unitString = [str substringWithRange:unitGroup];
-                        [self.responseData[rid] setObject:unitString forKey:@"unit"];
-                        NSLog(@"valueString %@", valueString);
-                        // format as number
-                        float valueFloat = atof([valueString UTF8String]);
-                        valueString = [[[NSNumber numberWithFloat:valueFloat] stringValue] mutableCopy];
-                        NSLog(@"valueString %@", valueString);
-                        
-                        // and append unit
-                        [valueString appendString:@" "];
-                        [valueString appendString:unitString];
-                        
-                        [self.responseData[rid] setObject:valueString forKey:@"value"];
+                    // parse data
+                    NSString *str = [[NSString alloc] initWithData:dataBlock encoding:NSUTF8StringEncoding];
+                    NSError *regexError;
+                    NSRegularExpression *regex = [NSRegularExpression regularExpressionWithPattern:@"(.*?)[(](.*?)(?:[*](.*?))?[)]" options:0 error:&regexError];
+                    if (regexError) {
+                        NSLog(@"%@",regexError.localizedDescription);
                     }
-                    else {
-                        [self.responseData[rid] setObject:valueString forKey:@"value"];
+                    
+                    NSArray* matches = [regex matchesInString:str options:0 range:NSMakeRange(0, str.length)];
+                    for (NSTextCheckingResult *match in matches) {
+                        NSRange ridGroup = [match rangeAtIndex:1];
+                        NSRange valueGroup = [match rangeAtIndex:2];
+                        NSRange unitGroup = [match rangeAtIndex:3];
+                        
+                        if (ridGroup.length && valueGroup.length) {
+                            NSString *rid = [str substringWithRange:ridGroup];
+                            NSMutableString *valueString = [[str substringWithRange:valueGroup] mutableCopy];
+                            [self.responseData setObject:[[NSMutableDictionary alloc] init] forKey:rid];
+                            
+                            if (unitGroup.length) {
+                                NSString *unitString = [str substringWithRange:unitGroup];
+                                [self.responseData[rid] setObject:unitString forKey:@"unit"];
+                                NSLog(@"valueString %@", valueString);
+                                // format as number
+                                float valueFloat = atof([valueString UTF8String]);
+                                valueString = [[[NSNumber numberWithFloat:valueFloat] stringValue] mutableCopy];
+                                NSLog(@"valueString %@", valueString);
+                                
+                                // and append unit
+                                [valueString appendString:@" "];
+                                [valueString appendString:unitString];
+                                
+                                [self.responseData[rid] setObject:valueString forKey:@"value"];
+                            }
+                            else {
+                                [self.responseData[rid] setObject:valueString forKey:@"value"];
+                            }
+                        }
                     }
+                    errorReceiving = NO;
+                }
+                else {
+                    NSLog(@"bcc error");
+                    errorReceiving = YES;
+                    frameReceived = NO;
                 }
             }
-            errorReceiving = NO;
-        }
-        else {
-            NSLog(@"bcc error");
-            errorReceiving = YES;
-            frameReceived = NO;
+            else {
+                // this meter sends data separate from ack
+                frameReceived = YES;
+            }
         }
     }
     else {
